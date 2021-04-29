24d368435d9e4d1c1b181cc5aecffd4c806a5151
liqin.zxl <liqin.zxl@alibaba-inc.com>
2021-04-29 22:55:41 +0800

    javaagent

---
+++ java-agent/src/main/java/com/myself/java/agent/AfterAgent.java
@@ -0,0 +1,10 @@
+package com.myself.java.agent;
+
+/**
+ * 功能描述: AfterAgent
+ *
+ * @author linqin.zxl
+ * @date 2021/4/29
+ */
+public class AfterAgent {
+}
---
+++ java-agent/src/main/java/com/myself/java/agent/PreAgent.java
@@ -0,0 +1,10 @@
+package com.myself.java.agent;
+
+/**
+ * 功能描述: PreAgent
+ *
+ * @author linqin.zxl
+ * @date 2021/4/29
+ */
+public class PreAgent {
+}
