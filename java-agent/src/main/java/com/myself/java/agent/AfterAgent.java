87a5625f1f93875cdd61e4f8d57400a051da7002
liqin.zxl <liqin.zxl@alibaba-inc.com>
2021-04-30 09:56:49 +0800

    landing

--- java-agent/src/main/java/com/myself/java/agent/AfterAgent.java
+++ java-agent/src/main/java/com/myself/java/agent/AfterAgent.java
@@ -1,10 +1,22 @@
 package com.myself.java.agent;
 
+import com.myself.logger.LoggerUtil;
+
+import java.lang.instrument.Instrumentation;
+
 /**
  * 功能描述: AfterAgent
- *
+ * 在主方法执行后调用
  * @author linqin.zxl
  * @date 2021/4/29
  */
 public class AfterAgent {
+
+    public static void afterAgent(String args, Instrumentation instrumentation){
+        LoggerUtil.info("****args******："+args);
+        Class[] allLoadedClasses = instrumentation.getAllLoadedClasses();
+        for (Class loadClass : allLoadedClasses) {
+            LoggerUtil.info("loadClass is:"+loadClass.getCanonicalName());
+        }
+    }
 }
--- java-agent/src/main/java/com/myself/java/agent/PreAgent.java
+++ java-agent/src/main/java/com/myself/java/agent/PreAgent.java
@@ -1,10 +1,22 @@
 package com.myself.java.agent;
 
+
+import java.lang.instrument.Instrumentation;
+
 /**
  * 功能描述: PreAgent
- *
+ * 在主方法main启动之前调用
+ * 执行方式：java -javaagent:/Users/zxl/workspace/work/study/code/landing/java-agent/target/java-agent-1.0-SNAPSHOT.jar=你好 -jar /Users/zxl/workspace/work/study/code/landing/spring-boot/target/spring-boot-1.0-SNAPSHOT.jar
  * @author linqin.zxl
  * @date 2021/4/29
  */
 public class PreAgent {
+
+    public static void premain(String args, Instrumentation instrumentation){
+        System.out.println("****args******："+args);
+        Class[] allLoadedClasses = instrumentation.getAllLoadedClasses();
+        for (Class loadClass : allLoadedClasses) {
+            System.out.println("loadClass is:"+loadClass.getCanonicalName());
+        }
+    }
 }
